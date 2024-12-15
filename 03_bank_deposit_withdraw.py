32a6525705de1773c501998a10b53c3f7bcf58e8
professor512 <karansalunkhe180@gmail.com>
2024-12-15 11:45:18 +0530

    Update 03_bank_deposit_withdraw.py

--- 03_bank_deposit_withdraw.py
+++ 03_bank_deposit_withdraw.py
@@ -1,33 +1,32 @@
-def deposit(num):
-    global balance
-    balance += num
-    
-def withdrawal(num):
-    global balance
-    if balance >= num:
-        balance -= num
-    else:
-        print("Withdrawal not possible because balance is insufficient.")
-        
-balance = 0
+# Write a Python program that computes the net amount of a bank account based a 
+# transaction log from console input. The transaction log format is shown as 
+# following: D 100 W 200 (Withdrawal is not allowed if balance is going negative. 
+# Write functions for withdraw and deposit) D means deposit while W means 
+# withdrawal. Suppose the following input is supplied to the program:
+# D 300, D 300 , W 200, D 100 Then, the output should be: 500
 
+def deposit(amount):
+    global balance
+    balance += amount
+
+def withdraw(amount):
+    global balance
+    if amount > balance:
+        print("Insufficient balance to withdraw")
+    else:
+        balance -= amount
+
+balance = 0 
 
 while True:
-    data = input("Please enter the transaction details (or type 'Exit' to end): ")
+    data = input("\nEnter Transaction Details: ")
     
-    if data.lower() == 'exit':
-        break
-    try:
-        action, amount_str = data.split()
-        amount = int(amount_str)
-        
-        if action.upper() == 'D':
-            deposit(amount)
-        elif action.upper() == 'W':
-            withdrawal(amount)
-        else:
-            print("Invalid transaction type. Use 'D' for deposit and 'W' for withdrawal.")
-        print("Balance =", balance)
-    
-    except ValueError:
-        print("Invalid input format. Please enter in the format 'D amount' or 'W amount'.")
+    action, amount = data.split()
+    amount = int(amount)
+    if action.upper() == 'D':
+        deposit(amount)
+    elif action.upper() == 'W':
+        withdraw(amount)
+    else:
+        print("\nInvalid Input Format")
+    print("\nTotal In Bank", balance)
